fa316adc93902e46e6b2ebf5840655f8b6673f4a
Bert Belder <bertbelder@gmail.com>
2019-12-08 13:00:53 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::default::Default;
 use std::fmt::Debug;
 use std::marker::PhantomData;
 use std::marker::Sized;
@@ -41,6 +40,18 @@ struct Opaque([u8; 0]);
 
 #[repr(C)]
 pub struct Isolate(Opaque);
+#[repr(C)]
+pub struct Context(Opaque);
+
+impl Context {
+    pub unsafe fn get_isolate(&self) -> *mut Isolate {
+        std::ptr::null_mut()
+    }
+
+    pub fn new() -> Self {
+        Self(unsafe { std::mem::transmute([0; 0]) })
+    }
+}
 
 impl Isolate {
     pub fn new() -> Self {
@@ -50,6 +61,8 @@ impl Isolate {
 
 mod scope {
     use super::*;
+    use std::ptr::null;
+    use std::ptr::null_mut;
 
     pub trait Scope
     where
@@ -60,37 +73,20 @@ mod scope {
         }
     }
 
-    pub trait ScopeImpl
-    where
-        Self: Sized,
-    {
-        fn cxx_isolate_private(&self) -> *mut Isolate;
-        fn cxx_isolate(guard: &mut Guard<'_, Self>) -> *mut Isolate {
-            guard.0.cxx_isolate_private()
-        }
-    }
-
     pub struct Guard<'a, T>(&'a T);
 
     impl<'a, T> Guard<'a, T> {
         fn new(scope: &'a T) -> Self {
             Self(scope)
         }
-    }
 
-    pub trait GuardImpl {
-        type Inner: ScopeImpl;
-        fn inner(&self) -> &Self::Inner;
-    }
-
-    impl<'a, T> GuardImpl for Guard<'a, T>
-    where
-        T: ScopeImpl,
-    {
-        type Inner = T;
-        fn inner(&self) -> &Self::Inner {
+        fn inner(&self) -> &T {
             &self.0
         }
+    }
+
+    impl<'a, T> Drop for Guard<'a, T> {
+        fn drop(&mut self) {}
     }
 
     pub struct Locker<'i> {
@@ -111,72 +107,101 @@ mod scope {
 
     impl<'i> Scope for Locker<'i> {}
 
-    impl<'i> ScopeImpl for Locker<'i> {
-        fn cxx_isolate_private(&self) -> *mut Isolate {
-            self.isolate as *const _ as *mut Isolate
+    pub trait LockerImpl
+    where
+        Self: Sized,
+    {
+        fn locked_cxx_isolate(guard: &mut Guard<Self>) -> *mut Isolate;
+    }
+
+    impl<'i> LockerImpl for Locker<'i> {
+        fn locked_cxx_isolate(guard: &mut Guard<Self>) -> *mut Isolate {
+            guard.inner().isolate as *const _ as *mut Isolate
         }
     }
 
-    pub trait LockerImpl: ScopeImpl {}
-    impl<'i> LockerImpl for Locker<'i> {}
-
-    pub trait LockerGuard: GuardImpl {}
-    impl<'sc, L> LockerGuard for Guard<'sc, L> where L: LockerImpl {}
-
-    pub struct HandleScope<'sc, G>
-    where
-        G: LockerGuard,
-    {
-        _data: [usize; 3],
-        parent: &'sc mut G,
+    pub struct HandleScope<'sc> {
+        isolate: *mut Isolate,
+        _prev_next: *mut Opaque,
+        _prev_limit: *mut Opaque,
+        _parent_scope: PhantomData<&'sc ()>,
     }
 
-    impl<'sc, G> HandleScope<'sc, G>
-    where
-        G: LockerGuard,
-    {
-        pub fn new(parent: &'sc mut G) -> Self {
+    impl<'sc> HandleScope<'sc> {
+        pub fn new<L>(parent_scope: &'sc mut Guard<'_, L>) -> Self
+        where
+            L: LockerImpl,
+        {
             Self {
-                parent,
-                _data: Default::default(),
+                isolate: LockerImpl::locked_cxx_isolate(parent_scope),
+                _prev_next: null_mut(),
+                _prev_limit: null_mut(),
+                _parent_scope: PhantomData,
             }
         }
     }
 
-    impl<'sc, G> Scope for HandleScope<'sc, G> where G: LockerGuard {}
+    impl<'sc> Scope for HandleScope<'sc> {}
 
-    impl<'sc, G> ScopeImpl for HandleScope<'sc, G>
-    where
-        G: LockerGuard,
-    {
-        fn cxx_isolate_private(&self) -> *mut Isolate {
-            self.parent.inner().cxx_isolate_private()
+    impl<'sc> LockerImpl for HandleScope<'sc> {
+        fn locked_cxx_isolate(guard: &mut Guard<Self>) -> *mut Isolate {
+            guard.inner().isolate as *const _ as *mut Isolate
         }
     }
-    impl<'sc, G> LockerImpl for HandleScope<'sc, G> where G: LockerGuard {}
 
     pub trait HandleScopeImpl: LockerImpl {}
-    impl<'sc, G> HandleScopeImpl for HandleScope<'sc, G> where G: LockerGuard {}
+    impl<'sc> HandleScopeImpl for HandleScope<'sc> {}
 
-    pub trait HandleScopeGuard<'sc> {}
-    impl<'sc, S> HandleScopeGuard<'sc> for Guard<'sc, S> where S: HandleScopeImpl {}
-
-    pub struct Local<'sc, T>
-    where
-        T: Copy + Debug,
-    {
-        value: T,
-        scope: PhantomData<&'sc ()>,
+    pub struct ContextScope<'l, 'sc, P> {
+        context: Local<'l, Context>,
+        _parent_scope: PhantomData<&'sc P>,
     }
 
-    impl<'sc, T> Local<'sc, T>
+    impl<'l, 'sc, P> ContextScope<'l, 'sc, P>
     where
-        T: Copy + Debug,
+        P: LockerImpl,
     {
-        pub fn new(_scope: &'_ mut impl HandleScopeGuard<'sc>, value: T) -> Self {
+        pub fn new(_parent_scope: &'_ mut Guard<'sc, P>, context: Local<'l, Context>) -> Self {
             Self {
-                value,
-                scope: PhantomData,
+                context,
+                _parent_scope: PhantomData,
+            }
+        }
+    }
+
+    impl<'l, 'sc, P> Scope for ContextScope<'l, 'sc, P> where P: LockerImpl {}
+
+    impl<'l, 'sc, P> LockerImpl for ContextScope<'l, 'sc, P>
+    where
+        P: LockerImpl,
+    {
+        fn locked_cxx_isolate(guard: &mut Guard<Self>) -> *mut Isolate {
+            unsafe { (*guard.inner().context.value).get_isolate() as *const _ as *mut Isolate }
+        }
+    }
+
+    impl<'l, 'sc, P> HandleScopeImpl for ContextScope<'l, 'sc, P> where P: HandleScopeImpl {}
+
+    pub struct Local<'sc, T> {
+        value: *const T,
+        _scope: PhantomData<&'sc ()>,
+    }
+
+    impl<'sc, T> Copy for Local<'sc, T> {}
+    impl<'sc, T> Clone for Local<'sc, T> {
+        fn clone(&self) -> Self {
+            *self
+        }
+    }
+
+    impl<'sc, T> Local<'sc, T> {
+        pub fn new<SC>(_scope: &'_ mut Guard<'sc, SC>, _value: T) -> Self
+        where
+            SC: HandleScopeImpl,
+        {
+            Self {
+                value: null(),
+                _scope: PhantomData,
             }
         }
 
@@ -188,10 +213,17 @@ mod scope {
         }
     }
 }
+use scope::ContextScope;
 use scope::HandleScope;
 use scope::Local;
 use scope::Locker;
 use scope::Scope;
+
+pub trait Varia {
+    type A;
+    type B;
+    type C;
+}
 
 fn main() {
     let isolate = Isolate::new();
@@ -200,20 +232,41 @@ fn main() {
     let mut data = HandleScope::new(&mut lock_guard);
     let mut scope = data.enter();
     let l1 = Local::new(&mut scope, 1);
+    let ctx = Local::new(&mut scope, Context::new());
     let mut l2 = Local::new(&mut scope, 2);
     {
         let mut data2 = HandleScope::new(&mut scope);
+        //let mut data3 = HandleScope::new(&mut scope);
+        //let k = scope.iii();
+        //std::mem::drop(data2);
+        //scope.iii();
         let mut scope2 = data2.enter();
+        let mut data3 = HandleScope::new(&mut scope2);
+        let mut scope3 = data3.enter();
+        let l22 = Local::new(&mut scope3, 22);
+        //let l11 = Local::new(&mut scope, 22);
+        //drop(scope3);
+        //drop(data2);
+        // drop(scope2);
+        l22.print();
+        //l22.print_mut();
+        /*
         let l3 = Local::new(&mut scope2, 3);
         let mut l4 = Local::new(&mut scope2, 4);
-        l1.print();
-        l2.print_mut();
         l3.print();
         l4.print_mut();
         let mut scope3 = HandleScope::new(&mut scope2);
         let mut scope3 = scope3.enter();
         let mut l6 = Local::new(&mut scope3, 11);
-        l6.print_mut();
+        l6.print_mut();*/
+        l1.print();
+        l2.print_mut();
+
+        let mut scope4a = ContextScope::new(&mut scope3, ctx);
+        let mut scope4b = scope4a.enter();
+        let mut l4a = Local::new(&mut scope4b, 22);
+        drop(scope4b);
+        l4a.print_mut();
     }
     let l5 = Local::new(&mut scope, 9);
     l5.print();
