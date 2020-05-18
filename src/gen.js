ac9ba09ca3c760a62171d0b63b171d6fbc2c5f30
Bert Belder <bertbelder@gmail.com>
2020-05-18 12:28:06 +0200

    Ok, real progress

--- src/gen.js
+++ src/gen.js
@@ -9,21 +9,21 @@ class Chain extends Array {
     return new Chain(this.lt, ...this);
   }
   find(kind) {
-    return this.filter((s) => s.kind === kind).pop();
+    return this.filter(s => s.kind === kind).pop();
   }
   find_front(kind) {
     return this[0]?.kind === kind ? this[0] : null;
   }
   remove(kind) {
-    return new Chain(this.lt, ...this.filter((s) => s.kind !== kind));
+    return new Chain(this.lt, ...this.filter(s => s.kind !== kind));
   }
   get_default_parent() {
     if (
-      (this.length === 1 && this[0].kind === "Handle") ||
-      (this.length === 2 &&
-        this[0].kind === "Handle" &&
-        this[1].kind === "Escape") ||
-      (this.length === 1 && this[0].kind === "TryCatch")
+      (this.length >= 1 && this[this.length - 1].kind === "Handle") ||
+      (this.length >= 2 &&
+        this[this.length - 2].kind === "Handle" &&
+        this[this.length - 1].kind === "Escape") ||
+      (this.length >= 1 && this[this.length - 1].kind === "TryCatch")
     ) {
       return { kind: "Context" };
     }
@@ -47,7 +47,7 @@ class Chain extends Array {
     const r = new Chain(
       lt,
       ...p,
-      ...this.filter((s) => s.kind !== "Handle" && s.kind !== "TryCatch")
+      ...this.filter(s => s.kind !== "Handle" && s.kind !== "TryCatch")
     );
     return [p, r];
   }
@@ -65,8 +65,7 @@ class Chain extends Array {
       lt,
       ...p,
       ...this.filter(
-        (s) =>
-          s.kind !== "Handle" && s.kind !== "Escape" && s.kind !== "TryCatch"
+        s => s.kind !== "Handle" && s.kind !== "Escape" && s.kind !== "TryCatch"
       )
     );
     return [p, r];
@@ -77,7 +76,7 @@ class Chain extends Array {
     if (this.find_front("TryCatch")) return; // No immediate nesting of TryCatch blocks.
     const lt = this.lt + 1;
     const p = new Chain(lt, { kind: "TryCatch", lt });
-    const r = new Chain(lt, ...p, ...this.filter((s) => s.kind !== "TryCatch"));
+    const r = new Chain(lt, ...p, ...this.filter(s => s.kind !== "TryCatch"));
     return [p, r];
   }
   *add_all() {
@@ -104,26 +103,27 @@ class Chain extends Array {
   deref() {
     let r = this.try_deref();
     if (r.length === 0) return;
-    r.lt = Math.max(0, ...r.map((s) => s.lt).filter(Boolean));
+    r.lt = Math.max(0, ...r.map(s => s.lt).filter(Boolean));
     return r;
   }
   get_lts() {
-    return [this.lt, ...this].filter((s) => s.lt != null);
+    return [this.lt, ...this].filter(s => s.lt != null);
   }
   gather_lts(lt_set) {
-    this.get_lts().forEach((s) => lt_set.add(s.lt));
+    this.get_lts().forEach(s => lt_set.add(s.lt));
   }
-  serialize(named_lts = name_lts(this)) {
-    let elide_if_default = (p, d) => (p !== d?.kind ? `, ${p}` : ``);
+  serialize(named_lts = name_lts(this), inner_ns = "active::") {
+    let elide_if_default = (p, d) =>
+      p !== d?.kind ? `, ${ns(p, inner_ns)}` : ``;
     if (this.length === 0) return;
     let a = this;
     let scope = a.find("Context") ? "Context" : "()";
     a = a.remove("Context");
-    let e = a.find("Escape");
-    a = a.remove("Escape");
     let h_default_parent = a.get_default_parent();
     let h = a.find("Handle");
     a = a.remove("Handle");
+    let e = a.find("Escape");
+    a = a.remove("Escape");
     scope = e
       ? `EscapableHandleScope<${named_lts.get(h.lt)}, ${named_lts.get(
           e.lt
@@ -150,12 +150,12 @@ class Chain extends Array {
 
 function name_lts(...chains) {
   let lts = new Set();
-  chains.forEach((c) => c.gather_lts(lts));
+  chains.forEach(c => c.gather_lts(lts));
   let numbered_lts = [...lts].sort((a, b) => b - a);
   return new Map(
     numbered_lts.map((lt, index) => [
       lt,
-      "'" + String.fromCharCode("a".charCodeAt(0) + index),
+      "'" + String.fromCharCode("a".charCodeAt(0) + index)
     ])
   );
 }
@@ -163,13 +163,24 @@ function name_lts(...chains) {
 function serialize_lts(named_lts, constrain = true) {
   let prev_lt;
   return [...named_lts.keys()]
-    .map((lt) => {
+    .map(lt => {
       let constraint =
         constrain && prev_lt != null ? named_lts.get(prev_lt) : null;
       prev_lt = lt;
       return named_lts.get(lt) + (constraint ? `: ${constraint}` : ``);
     })
     .join(", ");
+}
+
+function ns(scope, prefix) {
+  switch (scope) {
+    case "":
+    case "()":
+    case "Context":
+      return scope;
+    default:
+      return prefix + scope;
+  }
 }
 
 const chain_map = new Map();
@@ -180,7 +191,7 @@ const deref_map = new Map();
 for (let c1 of chain_map.values()) {
   for (let c2; (c2 = c1.deref()) != null; c1 = c2) {
     const named_lts = name_lts(c1, c2);
-    let kv = [c1, c2].map((c) => c.serialize(named_lts)).filter(Boolean);
+    let kv = [c1, c2].map(c => c.serialize(named_lts)).filter(Boolean);
     if (kv.length < 2) continue;
     const [k, v] = kv;
     if (deref_map.has(k)) {
@@ -194,29 +205,26 @@ console.log([...deref_map].map(([k, v]) => `${k} => ${v}`));
 
 console.log();
 
-const mappings = [];
+let mappings = Object.create(null);
 for (const c1 of chain_map.values()) {
   for (let [a, c2] of [...c1.add_all()].filter(Boolean)) {
     a = a.append_default_parent();
-    const named_lts = name_lts(c1, c2);
-    let [sc1, sc2] = [c1, c2]
-      .map((c) => c.serialize(named_lts))
-      .filter(Boolean);
-    if (!(sc1 && sc2)) continue;
     let sa = a.serialize();
-    //impl<'a, 'b: 'a> DerivedScope<'a, HandleScope<'b, Context>> for TryCatch<'a> {
-    //    type Alloc = alloc::TryCatch<'a, HandleScope<'b, Context>>;
-    //}
-
-    //console.log(`${cxxx[0]} + ${cxxx[1]} => ${cxxx[2]}`);
+    const named_lts = name_lts(c1, c2, a);
+    let [sc1, sc2] = [c1, c2].map(c => c.serialize(named_lts)).filter(Boolean);
+    if (!(sc1 && sc2)) continue;
     let code =
       [
         `impl<${serialize_lts(named_lts)}> DerivedScope<${named_lts.get(
           c2.lt
-        )}, ${sc1}> for ${sa} {`,
-        `  type NewScope = alloc::${sc2};`,
-        `}`,
+        )}, ${ns(sc1, "active::")}> for ${ns(sa, "active::")} {`,
+        `  type NewScope = ${ns(sc2, "alloc::")};`,
+        `}`
       ].join("\n") + "\n";
-    console.log(code);
+    (mappings[sa] ?? (mappings[sa] = [])).push(code);
   }
 }
+mappings = Object.entries(mappings)
+  .map(([k, v]) => [`\n// ===== ${k} =====\n`, ...v])
+  .flat(1);
+console.log(mappings.join("\n"));
--- src/main.rs
+++ src/main.rs
@@ -4,304 +4,341 @@ use std::marker::PhantomData as __;
 use std::ptr::null_mut;
 
 pub struct Context {
-    ptr: *mut (),
+  ptr: *mut (),
 }
 impl Context {
-    fn new() -> Self {
-        Self { ptr: null_mut() }
-    }
+  fn new() -> Self {
+    Self { ptr: null_mut() }
+  }
 }
 
 pub struct Local<'a, T> {
-    ptr: *mut (),
-    _phantom: __<&'a T>,
+  ptr: *mut (),
+  _phantom: __<&'a T>,
 }
 impl<'a, T> Local<'a, T> {
-    pub fn new(_: &'_ mut HandleScope<'a>) -> Self {
-        Self {
-            ptr: null_mut(),
-            _phantom: __,
-        }
+  pub fn new(_: &'_ mut HandleScope<'a>) -> Self {
+    Self {
+      ptr: null_mut(),
+      _phantom: __,
     }
+  }
 }
 
 pub trait DerivedScope<'a, P> {
-    type NewScope;
+  type NewScope;
 }
 
 impl<'a> DerivedScope<'a, Context> for HandleScope<'a> {
-    type NewScope = alloc::HandleScope<'a, Context>;
+  type NewScope = alloc::HandleScope<'a, Context>;
 }
 
-impl<'a> DerivedScope<'a, HandleScope<'a, ()>> for Context {
-    type NewScope = alloc::HandleScope<'a>;
+// ===== Context =====
+
+impl<'a> DerivedScope<'a, active::HandleScope<'a, ()>> for Context {
+  type NewScope = alloc::HandleScope<'a>;
 }
 
-impl<'a> DerivedScope<'a, HandleScope<'a>> for Context {
-    type NewScope = alloc::HandleScope<'a>;
+impl<'a> DerivedScope<'a, active::HandleScope<'a>> for Context {
+  type NewScope = alloc::HandleScope<'a>;
 }
 
-impl<'a, 'b: 'a> DerivedScope<'a, HandleScope<'b>> for HandleScope<'a> {
-    type NewScope = alloc::HandleScope<'a>;
-}
-
-impl<'a, 'b: 'a> DerivedScope<'a, HandleScope<'b>> for EscapableHandleScope<'a, 'b> {
-    type NewScope = alloc::EscapableHandleScope<'a, 'b>;
-}
-
-impl<'a, 'b: 'a> DerivedScope<'a, HandleScope<'b>> for TryCatch<'a> {
-    type NewScope = alloc::TryCatch<'a, HandleScope<'b, Context>>;
-}
-
-impl<'a, 'b: 'a> DerivedScope<'a, EscapableHandleScope<'a, 'b>> for Context {
-    type NewScope = alloc::EscapableHandleScope<'a, 'b>;
-}
-
-impl<'a, 'b: 'a, 'c: 'b> DerivedScope<'a, EscapableHandleScope<'b, 'c>> for HandleScope<'a> {
-    type NewScope = alloc::EscapableHandleScope<'a, 'c>;
-}
-
-impl<'a, 'b: 'a, 'c: 'b> DerivedScope<'a, EscapableHandleScope<'b, 'c>>
-    for EscapableHandleScope<'a, 'b>
+impl<'a, 'b: 'a> DerivedScope<'a, active::EscapableHandleScope<'a, 'b>>
+  for Context
 {
-    type NewScope = alloc::EscapableHandleScope<'a, 'b>;
+  type NewScope = alloc::EscapableHandleScope<'a, 'b>;
 }
 
-impl<'a, 'b: 'a, 'c: 'b> DerivedScope<'a, EscapableHandleScope<'b, 'c>> for TryCatch<'a> {
-    type NewScope = alloc::TryCatch<'a, EscapableHandleScope<'b, 'c, Context>>;
-}
-
-impl<'a, 'b: 'a, 'c: 'b> DerivedScope<'a, TryCatch<'a, EscapableHandleScope<'b, 'c, Context>>>
-    for Context
+impl<'a, 'b: 'a, 'c: 'b>
+  DerivedScope<'a, active::TryCatch<'a, active::EscapableHandleScope<'b, 'c>>>
+  for Context
 {
-    type NewScope = alloc::TryCatch<'a, EscapableHandleScope<'b, 'c, Context>>;
+  type NewScope = alloc::TryCatch<'a, active::EscapableHandleScope<'b, 'c>>;
+}
+
+impl<'a, 'b: 'a> DerivedScope<'a, active::TryCatch<'a, active::HandleScope<'b>>>
+  for Context
+{
+  type NewScope = alloc::TryCatch<'a, active::HandleScope<'b>>;
+}
+
+// ===== HandleScope<'a> =====
+
+impl<'a, 'b: 'a> DerivedScope<'a, active::HandleScope<'b>>
+  for active::HandleScope<'a>
+{
+  type NewScope = alloc::HandleScope<'a>;
+}
+
+impl<'a, 'b: 'a, 'c: 'b> DerivedScope<'a, active::EscapableHandleScope<'b, 'c>>
+  for active::HandleScope<'a>
+{
+  type NewScope = alloc::EscapableHandleScope<'a, 'c>;
 }
 
 impl<'a, 'b: 'a, 'c: 'b, 'd: 'c>
-    DerivedScope<'a, TryCatch<'b, EscapableHandleScope<'c, 'd, Context>>> for HandleScope<'a>
+  DerivedScope<'a, active::TryCatch<'b, active::EscapableHandleScope<'c, 'd>>>
+  for active::HandleScope<'a>
 {
-    type NewScope = alloc::EscapableHandleScope<'a, 'd>;
+  type NewScope = alloc::EscapableHandleScope<'a, 'd>;
+}
+
+impl<'a, 'b: 'a, 'c: 'b>
+  DerivedScope<'a, active::TryCatch<'b, active::HandleScope<'c>>>
+  for active::HandleScope<'a>
+{
+  type NewScope = alloc::HandleScope<'a>;
+}
+
+// ===== EscapableHandleScope<'a, 'b> =====
+
+impl<'a, 'b: 'a> DerivedScope<'a, active::HandleScope<'b>>
+  for active::EscapableHandleScope<'a, 'b>
+{
+  type NewScope = alloc::EscapableHandleScope<'a, 'b>;
+}
+
+impl<'a, 'b: 'a, 'c: 'b> DerivedScope<'a, active::EscapableHandleScope<'b, 'c>>
+  for active::EscapableHandleScope<'a, 'b>
+{
+  type NewScope = alloc::EscapableHandleScope<'a, 'b>;
 }
 
 impl<'a, 'b: 'a, 'c: 'b, 'd: 'c>
-    DerivedScope<'a, TryCatch<'b, EscapableHandleScope<'c, 'd, Context>>>
-    for EscapableHandleScope<'a, 'b>
+  DerivedScope<'a, active::TryCatch<'b, active::EscapableHandleScope<'c, 'd>>>
+  for active::EscapableHandleScope<'a, 'b>
 {
-    type NewScope = alloc::EscapableHandleScope<'a, 'c>;
+  type NewScope = alloc::EscapableHandleScope<'a, 'c>;
 }
 
-impl<'a, 'b: 'a> DerivedScope<'a, TryCatch<'a, HandleScope<'b, Context>>> for Context {
-    type NewScope = alloc::TryCatch<'a, HandleScope<'b, Context>>;
+impl<'a, 'b: 'a, 'c: 'b>
+  DerivedScope<'a, active::TryCatch<'b, active::HandleScope<'c>>>
+  for active::EscapableHandleScope<'a, 'b>
+{
+  type NewScope = alloc::EscapableHandleScope<'a, 'c>;
 }
 
-impl<'a, 'b: 'a, 'c: 'b> DerivedScope<'a, TryCatch<'b, HandleScope<'c, Context>>>
-    for HandleScope<'a>
+// ===== TryCatch<'a> =====
+
+impl<'a, 'b: 'a> DerivedScope<'a, active::HandleScope<'b>>
+  for active::TryCatch<'a>
 {
-    type NewScope = alloc::HandleScope<'a>;
+  type NewScope = alloc::TryCatch<'a, active::HandleScope<'b>>;
 }
 
-impl<'a, 'b: 'a, 'c: 'b> DerivedScope<'a, TryCatch<'b, HandleScope<'c, Context>>>
-    for EscapableHandleScope<'a, 'b>
+impl<'a, 'b: 'a, 'c: 'b> DerivedScope<'a, active::EscapableHandleScope<'b, 'c>>
+  for active::TryCatch<'a>
 {
-    type NewScope = alloc::EscapableHandleScope<'a, 'c>;
+  type NewScope = alloc::TryCatch<'a, active::EscapableHandleScope<'b, 'c>>;
 }
 
 pub(self) mod data {
-    pub struct EscapeSlot(*const ());
-    pub struct HandleScope([usize; 3]);
-    pub struct EscapableHandleScope {
-        handle_scope: HandleScope,
-        escape_slot: EscapeSlot,
-    }
-    pub(crate) struct TryCatch([usize; 7]);
+  pub struct EscapeSlot(*const ());
+  pub struct HandleScope([usize; 3]);
+  pub struct EscapableHandleScope {
+    handle_scope: HandleScope,
+    escape_slot: EscapeSlot,
+  }
+  pub(crate) struct TryCatch([usize; 7]);
 
-    impl Drop for HandleScope {
-        fn drop(&mut self) {}
-    }
-    impl Drop for EscapableHandleScope {
-        fn drop(&mut self) {}
-    }
-    impl Drop for TryCatch {
-        fn drop(&mut self) {}
-    }
+  impl Drop for HandleScope {
+    fn drop(&mut self) {}
+  }
+  impl Drop for EscapableHandleScope {
+    fn drop(&mut self) {}
+  }
+  impl Drop for TryCatch {
+    fn drop(&mut self) {}
+  }
 }
 
 pub mod alloc {
-    use super::*;
-    pub enum HandleScope<'a, P = Context> {
-        Declared(&'a mut P),
-        Entered(data::HandleScope),
-    }
-    pub enum EscapableHandleScope<'a, 'b, P = Context> {
-        Declared {
-            parent: &'a mut P,
-            escape_sclot: active::EscapeSlot<'b>,
-        },
-        Entered(data::EscapableHandleScope),
-    }
-    pub enum TryCatch<'a, P = Context> {
-        Declared(&'a mut P),
-        Entered(data::HandleScope),
-    }
+  use super::*;
+  pub enum HandleScope<'a, P = Context> {
+    Declared(&'a mut P),
+    Entered(data::HandleScope),
+  }
+  pub enum EscapableHandleScope<'a, 'b, P = Context> {
+    Declared {
+      parent: &'a mut P,
+      escape_slot: active::EscapeSlot<'b>,
+    },
+    Entered(data::EscapableHandleScope),
+  }
+  pub enum TryCatch<'a, P = Context> {
+    Declared(&'a mut P),
+    Entered(data::HandleScope),
+  }
 
-    impl<'a> HandleScope<'a, ()> {
-        pub fn enter(&'a mut self) -> &'a mut active::HandleScope<'a, ()> {
-            unimplemented!()
-        }
+  impl<'a> HandleScope<'a, ()> {
+    pub fn enter(&'a mut self) -> &'a mut active::HandleScope<'a, ()> {
+      unimplemented!()
     }
-    impl<'a> HandleScope<'a, Context> {
-        pub fn enter(&'a mut self) -> &'a mut active::HandleScope<'a, Context> {
-            unimplemented!()
-        }
+  }
+  impl<'a> HandleScope<'a, Context> {
+    pub fn enter(&'a mut self) -> &'a mut active::HandleScope<'a, Context> {
+      unimplemented!()
     }
-    impl<'a, 'b> EscapableHandleScope<'a, 'b, Context> {
-        pub fn enter(&'a mut self) -> &'a mut active::EscapableHandleScope<'a, 'b, Context> {
-            unimplemented!()
-        }
+  }
+  impl<'a, 'b> EscapableHandleScope<'a, 'b, Context> {
+    pub fn enter(
+      &'a mut self,
+    ) -> &'a mut active::EscapableHandleScope<'a, 'b, Context> {
+      unimplemented!()
     }
-    impl<'a, 'b, 'c> TryCatch<'a, active::EscapableHandleScope<'b, 'c, Context>> {
-        pub fn enter(
-            &'a mut self,
-        ) -> &'a mut active::TryCatch<'a, active::EscapableHandleScope<'b, 'c, Context>> {
-            unimplemented!()
-        }
+  }
+  impl<'a, 'b, 'c> TryCatch<'a, active::EscapableHandleScope<'b, 'c, Context>> {
+    pub fn enter(
+      &'a mut self,
+    ) -> &'a mut active::TryCatch<
+      'a,
+      active::EscapableHandleScope<'b, 'c, Context>,
+    > {
+      unimplemented!()
     }
-    impl<'a, 'b> TryCatch<'a, active::HandleScope<'b, Context>> {
-        pub fn enter(
-            &'a mut self,
-        ) -> &'a mut active::TryCatch<'a, active::HandleScope<'b, Context>> {
-            unimplemented!()
-        }
+  }
+  impl<'a, 'b> TryCatch<'a, active::HandleScope<'b, Context>> {
+    pub fn enter(
+      &'a mut self,
+    ) -> &'a mut active::TryCatch<'a, active::HandleScope<'b, Context>> {
+      unimplemented!()
     }
+  }
 }
 
 pub(self) mod active {
-    use super::*;
+  use super::*;
 
-    struct Common {
-        isolate: *mut (),
-    }
+  struct Common {
+    isolate: *mut (),
+  }
 
-    pub struct EscapeSlot<'a>(*const (), __<&'a mut ()>);
-    pub struct HandleScope<'a, P = Context> {
-        common: Common,
-        _phantom: __<&'a mut P>,
-    }
-    pub struct EscapableHandleScope<'a, 'b, P = Context> {
-        common: Common,
-        _phantom: __<(&'a mut P, &'b mut P)>,
-    }
-    pub struct TryCatch<'a, P = Context> {
-        common: Common,
-        _phantom: __<&'a mut P>,
-    }
+  pub struct EscapeSlot<'a>(*const (), __<&'a mut ()>);
+  pub struct HandleScope<'a, P = Context> {
+    common: Common,
+    _phantom: __<&'a mut P>,
+  }
+  pub struct EscapableHandleScope<'a, 'b, P = Context> {
+    common: Common,
+    _phantom: __<(&'a mut P, &'b mut P)>,
+  }
+  pub struct TryCatch<'a, P = Context> {
+    common: Common,
+    _phantom: __<&'a mut P>,
+  }
 
-    impl<'a> HandleScope<'a> {
-        pub fn root() -> alloc::HandleScope<'a, ()> {
-            unimplemented!()
-        }
-        pub fn new<'b: 'a, P: 'b>(_parent: &'a mut P) -> <Self as DerivedScope<P>>::NewScope
-        where
-            Self: DerivedScope<'a, P>,
-        {
-            unimplemented!()
-        }
+  impl<'a> HandleScope<'a> {
+    pub fn root() -> alloc::HandleScope<'a, ()> {
+      unimplemented!()
     }
-    impl<'a, 'b> EscapableHandleScope<'a, 'b> {
-        pub fn new<'c: 'a, P: 'c>(_parent: &'a mut P) -> <Self as DerivedScope<P>>::NewScope
-        where
-            Self: DerivedScope<'a, P>,
-        {
-            unimplemented!()
-        }
+    pub fn new<'b: 'a, P: 'b>(
+      _parent: &'a mut P,
+    ) -> <Self as DerivedScope<P>>::NewScope
+    where
+      Self: DerivedScope<'a, P>,
+    {
+      unimplemented!()
     }
-    impl<'a> TryCatch<'a> {
-        pub fn new<'b: 'a, P: 'b>(_parent: &'a mut P) -> <Self as DerivedScope<P>>::NewScope
-        where
-            Self: DerivedScope<'a, P>,
-        {
-            unimplemented!()
-        }
+  }
+  impl<'a, 'b> EscapableHandleScope<'a, 'b> {
+    pub fn new<'c: 'a, P: 'c>(
+      _parent: &'a mut P,
+    ) -> <Self as DerivedScope<P>>::NewScope
+    where
+      Self: DerivedScope<'a, P>,
+    {
+      unimplemented!()
     }
+  }
+  impl<'a> TryCatch<'a> {
+    pub fn new<'b: 'a, P: 'b>(
+      _parent: &'a mut P,
+    ) -> <Self as DerivedScope<P>>::NewScope
+    where
+      Self: DerivedScope<'a, P>,
+    {
+      unimplemented!()
+    }
+  }
 
-    impl<'a, P> Drop for HandleScope<'a, P> {
-        fn drop(&mut self) {}
-    }
-    impl<'a, 'b, P> Drop for EscapableHandleScope<'a, 'b, P> {
-        fn drop(&mut self) {}
-    }
-    impl<'a, P> Drop for TryCatch<'a, P> {
-        fn drop(&mut self) {}
-    }
+  impl<'a, P> Drop for HandleScope<'a, P> {
+    fn drop(&mut self) {}
+  }
+  impl<'a, 'b, P> Drop for EscapableHandleScope<'a, 'b, P> {
+    fn drop(&mut self) {}
+  }
+  impl<'a, P> Drop for TryCatch<'a, P> {
+    fn drop(&mut self) {}
+  }
 
-    impl<'a> HandleScope<'a, ()> {}
-    impl<'a> HandleScope<'a, Context> {}
-    impl<'a, 'b> EscapableHandleScope<'a, 'b, Context> {}
-    impl<'a, 'b, 'c> TryCatch<'a, EscapableHandleScope<'b, 'c, Context>> {}
-    impl<'a, 'b> TryCatch<'a, HandleScope<'b, Context>> {}
+  impl<'a> HandleScope<'a, ()> {}
+  impl<'a> HandleScope<'a, Context> {}
+  impl<'a, 'b> EscapableHandleScope<'a, 'b, Context> {}
+  impl<'a, 'b, 'c> TryCatch<'a, EscapableHandleScope<'b, 'c, Context>> {}
+  impl<'a, 'b> TryCatch<'a, HandleScope<'b, Context>> {}
 }
 
 use active::*;
 
 fn main() {
-    //let mut root = HandleScope::root();
-    //let _root = root.enter();
+  let mut root = HandleScope::root();
+  let _root = root.enter();
 
-    let mut ctx = Context::new();
+  let mut ctx = Context::new();
 
-    let mut s1 = HandleScope::new(&mut ctx);
-    let s1 = s1.enter();
+  let mut s1 = HandleScope::new(&mut ctx);
+  let s1 = s1.enter();
 
-    let _s1l1 = Local::<i8>::new(s1);
-    let _s1l2 = Local::<i8>::new(s1);
-    let _fail = {
-        let mut s2 = HandleScope::new(s1);
-        let s2 = s2.enter();
+  let _s1l1 = Local::<i8>::new(s1);
+  let _s1l2 = Local::<i8>::new(s1);
+  let _fail = {
+    let mut s2 = HandleScope::new(s1);
+    let s2 = s2.enter();
 
-        let s2l1 = Local::<i8>::new(s2);
-        let _s2l2 = Local::<i8>::new(s2);
-        //let _fail = Local::<i8>::new(s1);
-        s2l1;
-    };
-    let _s1l3 = Local::<i8>::new(s1);
+    let s2l1 = Local::<i8>::new(s2);
+    let _s2l2 = Local::<i8>::new(s2);
+    //let _fail = Local::<i8>::new(s1);
+    s2l1;
+  };
+  let _s1l3 = Local::<i8>::new(s1);
 
-    test1();
+  test1();
 }
 
 fn test1() {
-    let mut ctx = Context::new();
+  let mut ctx = Context::new();
 
-    let mut s1 = HandleScope::new(&mut ctx);
-    let s1 = s1.enter();
+  let mut s1 = HandleScope::new(&mut ctx);
+  let s1 = s1.enter();
 
+  {
+    let mut s2 = HandleScope::new(s1);
+    let _s2 = s2.enter();
+  }
+
+  {
+    let mut s2 = EscapableHandleScope::new(s1);
+    let s2 = s2.enter();
     {
-        let mut s2 = HandleScope::new(s1);
-        let _s2 = s2.enter();
+      let mut s3 = HandleScope::new(s2);
+      let _s3 = s3.enter();
     }
-
     {
-        let mut s2 = EscapableHandleScope::new(s1);
-        let s2 = s2.enter();
-        {
-            let mut s3 = HandleScope::new(s2);
-            let _s3 = s3.enter();
-        }
-        {
-            let mut s3 = TryCatch::new(s2);
-            let _s3 = s3.enter();
-        }
+      let mut s3 = TryCatch::new(s2);
+      let _s3 = s3.enter();
     }
+  }
 
+  {
+    let mut s2 = TryCatch::new(s1);
+    let s2 = s2.enter();
     {
-        let mut s2 = TryCatch::new(s1);
-        let s2 = s2.enter();
-        {
-            let mut s3 = HandleScope::new(s2);
-            let _s3 = s3.enter();
-        }
-        {
-            let mut s3 = EscapableHandleScope::new(s2);
-            let _s3 = s3.enter();
-        }
+      let mut s3 = HandleScope::new(s2);
+      let _s3 = s3.enter();
     }
+    {
+      let mut s3 = EscapableHandleScope::new(s2);
+      let _s3 = s3.enter();
+    }
+  }
 }
